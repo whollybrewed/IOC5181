21df3b2eadd01be03885863dfafe6fb9e95df0b8
whollybrewed <andylee47695@gmail.com>
2019-10-30 10:03:45 +0000

    pthread with srand as RNG and floting op

--- HW1/pi.c
+++ HW1/pi.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+typedef unsigned long long BIGINT;
+
+BIGINT total_num_hits;
+BIGINT toss_per_thread;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *dart_toss ()
+{
+	BIGINT num_hits = 0;
+	unsigned int state = rand();
+	for (BIGINT toss = 0; toss < toss_per_thread; toss++){
+		double x = rand_r(&state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
+		double y = rand_r(&state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
+		if ( x * x + y * y <= 1){
+			num_hits++;
+		}
+	}
+	pthread_mutex_lock(&mutex);
+	total_num_hits += num_hits;
+	pthread_mutex_unlock(&mutex);
+	pthread_exit(NULL);
+}
 
 int main(int argc, char **argv)
-{
-    double pi_estimate, distance_squared, x, y;
-    unsigned long long  number_of_cpu, number_of_tosses, number_in_circle, toss;
-    if ( argc < 2) {
-        exit(-1);
+{	
+	srand(time(NULL));
+	double pi_estimate;
+	unsigned long long  num_cpu, num_toss, num_thread;
+	if ( argc < 2) {
+		exit(-1);
+	}
+	num_cpu = atoi(argv[1]);
+	num_toss = atoi(argv[2]);
+	if (( num_cpu < 1) || ( num_toss < 0)) {
+		exit(-1);
+	}
+	num_thread = num_cpu;
+	toss_per_thread = num_toss / num_thread;
+	pthread_t tid[num_thread];
+    for(int i = 0; i < num_thread; i++){
+        int create_fail = pthread_create(&tid[i], NULL, dart_toss,(void*)NULL);
+        if (create_fail){
+            printf("pthread_create() error\n");
+            exit(-1);
+        }
     }
-    number_of_cpu = atoi(argv[1]);
-    number_of_tosses = atoi(argv[2]);
-    if (( number_of_cpu < 1) || ( number_of_tosses < 0)) {
-        exit(-1);
+    for (int i = 0; i < num_thread; i++) {
+        pthread_join(tid[i], NULL);
     }
-
-    number_in_circle = 0;
-    for (toss = 0; toss < number_of_tosses; toss++) {
-        // x = random double between -1 and 1;
-        // y = random double between -1 and 1;
-        distance_squared = x*x + y*y;
-        if (distance_squared <= 1)
-            number_in_circle++;
-    }
-    pi_estimate = 4*number_in_circle/((double) number_of_tosses);
-
-    printf("%f\n",pi_estimate);
-    return 0;
+	pthread_mutex_destroy(&mutex);
+	pi_estimate = 4 * total_num_hits/((double)num_toss);
+	printf("%f\n", pi_estimate);
+	return 0;
 }
